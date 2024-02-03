9acf939881e7b6381c5f2ab14a30061d48037a27
ashwithags <ashwithags@gmail.com>
2024-02-03 17:21:54 +0530

    index page update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { ProductsComponent } from './products/products.component';
@@ -11,7 +13,11 @@ import { CategoryComponent } from './category/category.component';
 import { CartComponent } from './cart/cart.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-import { ProductGridComponent } from './product-grid/product-grid.component';
+import { SortPipe } from './sort.pipe';
+import { FilterOnPricePipe } from './filter-on-price.pipe';
+import { ProductsGridComponent } from './Shared/products-grid/products-grid.component';
+import { AdminComponent } from './admin/admin.component';
+import { EmployeeComponent } from './admin/employee/employee.component';
 
 @NgModule({
   declarations: [
@@ -22,13 +28,18 @@ import { ProductGridComponent } from './product-grid/product-grid.component';
     CartComponent,
     HeaderComponent,
     HomeComponent,
-    ProductGridComponent
+    SortPipe,
+    FilterOnPricePipe,
+    ProductsGridComponent,
+    AdminComponent,
+    EmployeeComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NoopAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
