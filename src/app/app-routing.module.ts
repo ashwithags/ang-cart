46c2810c7cd09ba3c99c8df85781241298851e70
ashwithags <ashwithags@gmail.com>
2024-02-03 17:16:47 +0530

    adding new routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,17 @@ import { CategoryComponent } from './category/category.component';
 import { CartComponent } from './cart/cart.component';
 import { HomeComponent } from './home/home.component';
 import { ProductComponent } from './products/product/product.component';
+import { EmployeeComponent } from './admin/employee/employee.component';
+import { AdminComponent } from './admin/admin.component';
 
 const routes: Routes = [
   {path:'products',component:ProductsComponent},
   {path:'product/:id', component:ProductComponent},
   {path:'category',component:CategoryComponent},
   {path:'cart', component:CartComponent},
-  {path:'',component:HomeComponent}
+  {path:'',component:HomeComponent},
+  {path:'employee', component:EmployeeComponent},
+  {path:'admin', component:AdminComponent}
 ];
 
 @NgModule({
