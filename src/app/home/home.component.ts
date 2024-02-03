d08546c7d4e723b94544db1bcec3574e30d3b5f2
ashwithags <ashwithags@gmail.com>
2024-02-03 17:20:27 +0530

    home page code

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoryService } from '../Service/category.service';
+import { ProductService } from '../Service/product.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public ProductCategory: string[] = [];
+  public ProductList: any[] = [];
+  public page: string = "Categories";
+
+  constructor(
+    private Catagory: CategoryService,
+    private Product: ProductService
+  ) { }
 
   ngOnInit(): void {
+
+    this.getCategory();
   }
+
+  getCategory() {
+    this.Catagory.getCategories().subscribe(
+      data => {
+        this.ProductCategory = data;
+        console.log(this.ProductCategory);
+        this.fetchProduct(this.ProductCategory[0]);
+      }
+    )
+  }
+
+  fetchProduct(category: string) {
+    this.Product.getProductCategory(category).subscribe(
+      data => {
+        this.ProductList = data.products;
+      }
+    )
+  }
+
+
+
 
 }
